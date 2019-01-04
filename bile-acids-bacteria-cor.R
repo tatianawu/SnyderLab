bf14063f19fc21c00bd8dad02627f52f822d399a
Tatiana Wu <tatiana.wu99@gmail.com>
2019-01-04 16:01:55 -0500

    Fix correlation matrix and attach p-values

--- bile-acids-bacteria-cor.R
+++ bile-acids-bacteria-cor.R
@@ -1,5 +1,3 @@
-# Correlations
-
 # library(openxlsx)
 bile_acids_processed <- read.xlsx("bile_acids_processed.xlsx")
 bugs_processed <- read.xlsx("bugs_list_genus_processed.xlsx")
@@ -15,6 +13,25 @@ bugs_and_bile_acids <- bugs_and_bile_acids[-1]
 bugs_and_bile_acids[] <- lapply(bugs_and_bile_acids, function(x) {
   as.numeric(as.character(x))
 })
+bugs_and_bile_acids <- na.omit(bugs_and_bile_acids)
 
-correlation_matrix <- cor(bugs_and_bile_acids, use="complete")
-# write.xlsx(correlation_matrix, "correlation_matrix.xlsx")
+
+# install.packages("Hmisc")
+# library(Hmisc)
+cor <- rcorr(format(t(bugs_and_bile_acids), digits=20), type="spearman")
+cor.data <- cor$r
+cor.data[upper.tri(cor.data, diag=T)] <- 0
+pval.data <- cor$P
+pval.data[upper.tri(pval.data, diag=T)] <- NA
+FDR.data <- apply(pval.data, 2, p.adjust, method="fdr", n=length(pval.data))
+pdf(paste("./lognormlipids", "fdr", "pval_hist.pdf", sep="_"))
+hist(pval.data, breaks=100, col="darkblue")
+dev.off()
+pdf(paste("./lognormlipids", "fdr", "FDR_hist.pdf", sep="_"))
+hist(FDR.data, breaks=100, col="darkblue")
+dev.off()
+pdf(paste("./lognormlipids", "fdr", "cor_hist.pdf", sep="_"))
+hist(cor.data, breaks=10, col="red")
+dev.off()
+cor.data[FDR.data > 0.05] = 0
+write.table(cor.data, file=paste("./lognormlipids", "fdr", "cor_data.txt", sep="_"), sep="\t", col.names=NA)
